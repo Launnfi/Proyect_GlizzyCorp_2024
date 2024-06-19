0727a33c0996846d888303dad7fa063b93a2fdb5
Launnfi <163862139+Launnfi@users.noreply.github.com>
2024-06-18 21:57:16 -0300

    carrucel añadiido y about us

--- modulos/inicio.php
+++ modulos/inicio.php
@@ -7,9 +7,22 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
+       <!-- Bootstrap CSS -->
+       <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+
 
     <style>
         /* Estilos opcionales para el header */
+        .carousel-caption-custom {
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            background: rgba(0, 0, 0, 0.5);
+            color: white;
+            padding: 10px;
+            text-align: center;
+        }
         header {
             background-color: rgb(252, 204, 245);
             color: black; 
@@ -25,19 +38,59 @@ session_start();
         .a{
             text-align: left;
         }
+        
     </style>
 </head>
 <body>
-    <header>
+<header>
         <nav>
-            <a name= "a" href="#seccion1">Inicio</a>
-            <a name= "a" href="#seccion2">buscar</a>
-            <a name= "a" href="#seccion3">sobre nosotros</a>
+            <a name= "a" href="inicio.php">Inicio</a>
+            <a name= "a" href="#seccion2">categorias</a>
+            <a name= "a" href="about_us.php">sobre nosotros</a>
         </nav>
     </header>
-<div id="unocoma">
+    <div id="unocoma">
         </div>
         <div id="salir"> <a href="logout.php">Toca aqui para Salir</a> </div>
+<?php
+// Datos del carrusel (pueden provenir de una base de datos)
+$images = [
+    ["src" => "https://www.anahuac.mx/mexico/sites/default/files/styles/webp/public/noticias/Los-colores-que-utilizamos-en-la-ropa-dicen-como-somos.jpg.webp?itok=k3GFCGkN", "description" => "Descripción de la imagen 1"],
+    ["src" => "https://i.ebayimg.com/images/g/KRsAAOSwJoZmSivs/s-l1600.webp", "description" => "Descripción de la imagen 2"],
+    ["src" => "https://i.ebayimg.com/images/g/ixIAAOSwW4tlVpV5/s-l1600.webp", "description" => "Descripción de la imagen 3"],
+];
+?>
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <?php foreach ($images as $index => $image): ?>
+            <li data-target="#carouselExampleIndicators" data-slide-to="<?php echo $index; ?>" class="<?php echo $index === 0 ? 'active' : ''; ?>"></li>
+        <?php endforeach; ?>
+    </ol>
+    <div class="carousel-inner">
+        <?php foreach ($images as $index => $image): ?>
+            <div class="carousel-item <?php echo $index === 0 ? 'active' : ''; ?>">
+                <img src="<?php echo $image['src']; ?>" class="d-block w-100" alt="Imagen <?php echo $index + 1; ?>">
+                <div class="carousel-caption-custom">
+                    <p><?php echo $image['description']; ?></p>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Anterior</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Siguiente</span>
+    </a>
+</div>
+
+<!-- Bootstrap JS y dependencias -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
     <!-- Contenido de la página -->
     <section id="seccion1">
