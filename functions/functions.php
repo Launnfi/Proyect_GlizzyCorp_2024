04699a76c364d5d9cf371cb1d7c22543201fe2d3
Kendrik Lamar <164172879+NicoSiffredo@users.noreply.github.com>
2024-09-17 20:45:42 -0300

    ategorias de productos dinamicas
    categorias de productos dinamicas en la seccion tienda

--- functions/functions.php
+++ functions/functions.php
@@ -238,3 +238,123 @@ function getPCatpro(){
         
     }
 }
+function getcatpro(){
+    
+    global $db;
+    
+    if(isset($_GET['cat'])){
+        
+        $cat_id = $_GET['cat'];
+        
+        $get_cat = "select * from categorias where cat_id='$cat_id'";
+        
+        $run_cat = mysqli_query($db,$get_cat);
+        
+        $row_cat = mysqli_fetch_array($run_cat);
+        
+        $cat_titulo = $row_cat['cat_titulo'];
+        
+        $cat_desc = $row_cat['cat_desc'];
+        
+        $get_cat = "select * from productos where cat_id='$cat_id'";
+        
+        $run_products = mysqli_query($db,$get_cat);
+        
+        $count = mysqli_num_rows($run_products);
+        
+        if($count==0){
+            
+            
+            echo "
+            
+                <div class='box'>
+                
+                    <h1> Aun no hay productos en esta categoria </h1>
+                
+                </div>
+            
+            ";
+            
+        }else{
+            
+            echo "
+            
+                <div class='box'>
+                
+                    <h1> $cat_titulo </h1>
+                    
+                    <p> $cat_desc </p>
+                
+                </div>
+            
+            ";
+            
+        }
+        
+        while($row_products=mysqli_fetch_array($run_products)){
+            
+            $pro_id = $row_products['producto_id'];
+            
+            $pro_titulo = $row_products['producto_titulo'];
+            
+            $pro_precio = $row_products['producto_precio'];
+            
+            $pro_desc = $row_products['producto_desc'];
+            
+            $pro_img1 = $row_products['producto_img1'];
+            
+            echo "
+            
+                <div class='col-md-4 col-sm-6 center-responsive'>
+                                    
+                    <div class='product'>
+                                        
+                        <a href='details.php?pro_id=$pro_id'>
+                                            
+                            <img class='img-responsive' src='admin_area/product_images/$pro_img1'>
+                                            
+                        </a>
+                                            
+                        <div class='text'>
+                                            
+                            <h3>
+                                                
+                                <a href='details.php?pro_id=$pro_id'> $pro_titulo </a>
+                                                
+                            </h3>
+                                            
+                        <p class='price'>
+
+                            $$pro_precio
+
+                        </p>
+
+                            <p class='buttons'>
+
+                                <a class='btn btn-default' href='details.php?pro_id=$pro_id'>
+
+                                Ver detalles
+
+                                </a>
+
+                                <a class='btn btn-primary' href='details.php?pro_id=$pro_id'>
+
+                                <i class='fa fa-shopping-cart'></i> Añadir al carrito
+
+                                </a>
+
+                            </p>
+                                            
+                        </div>
+                                        
+                    </div>
+                                    
+                </div>
+            
+            ";
+            
+        }
+        
+    }
+    
+}
