c50432ab65410a4c4939672d97fa928d7aa57f2d
Kendrik Lamar <164172879+NicoSiffredo@users.noreply.github.com>
2024-09-24 20:40:12 -0300

    Update vicentita.sql

--- vicentita.sql
+++ vicentita.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-09-2024 a las 01:30:23
+-- Tiempo de generación: 25-09-2024 a las 01:39:46
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
