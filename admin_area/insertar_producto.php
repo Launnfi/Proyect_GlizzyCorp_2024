bae9b003645aadd473a89a2d4783a39670e0af29
Launnfi <163862139+Launnfi@users.noreply.github.com>
2024-10-17 10:41:19 -0300

    n

--- admin_area/insertar_producto.php
+++ admin_area/insertar_producto.php
@@ -97,7 +97,7 @@ include("../db.php");
                               
                           </select>
                           
-                      </div>>
+                      </div>
                        
                    </div>
                    
