5c7855f971441d9d4af6fca1d6470240e3dacc88
Launnfi <163862139+Launnfi@users.noreply.github.com>
2024-10-18 22:17:20 -0300

    Panel del admin creado
    Panel de admin realizado con su styles y su navbar

---
+++ admin_area/includes/header.php
@@ -0,0 +1,233 @@
+<?php 
+session_start();
+include("db.php");
+include("functions/functions.php");
+
+?>
+
+<?php 
+
+// Verificamos si está presente el parámetro 'pro_id'
+if(isset($_GET['pro_id'])){
+
+    // Guardamos el valor de 'pro_id' que viene desde la URL en una variable
+    $producto_id = $_GET['pro_id'];
+    
+    // Consulta SQL para obtener los detalles del producto con el ID correspondiente
+    $get_product = "select * from productos where producto_id='$producto_id'";
+
+    $run_product = mysqli_query($con,$get_product);
+
+    $row_product = mysqli_fetch_array($run_product);
+
+    //obtiene cada detalle de los productos
+    $p_cat_id = $row_product['p_cat_id'];
+
+    $pro_titulo = $row_product['producto_titulo'];
+
+    $pro_price = $row_product['producto_precio'];
+
+    $pro_desc = $row_product['producto_desc'];
+
+    $pro_img1 = $row_product['producto_img1'];
+
+    $pro_img2 = $row_product['producto_img2'];
+
+    $pro_img3 = $row_product['producto_img3'];
+
+    // Consulta SQL para obtener detalles de la categoría del producto
+    $get_p_cat = "select * from productos_categorias where p_cat_id='$p_cat_id'";
+
+    $run_p_cat = mysqli_query($con,$get_p_cat);
+
+    $row_p_cat = mysqli_fetch_array($run_p_cat);
+
+    // Obtenemos el título de la categoría del producto
+    $p_cat_titulo = $row_p_cat['p_cat_titulo'];
+
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Vicenta</title>
+    <link rel="stylesheet" href="styles/bootstrap-337.min.css">
+    <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles/styles.css">
+</head>
+<body>
+   
+   <div id="top"><!-- Top empieza -->
+       
+       <div class="container"><!-- container empieza -->
+           
+           <div class="col-md-6 offer"><!-- col-md-6 offer empieza -->
+               
+                <?php 
+                   
+                   if(!isset($_SESSION['cliente_email'])){
+                       
+                       echo "Bienvenido: Invitado";
+                       
+                   }else{
+                       
+                       echo "Bienvenido: " . $_SESSION['cliente_email'] . "";
+                       
+                   }
+                   
+                ?>
+                   
+               <a href="cerrar_sesion.php"><?php items(); ?> Productos en tu carrito | Total: <?php echo mont_total(); ?> </a>
+               
+           </div><!-- col-md-6 offer termina -->
+           
+           <div class="col-md-6"><!-- col-md-6 empieza -->
+               
+               <ul class="menu"><!-- cmenu empieza -->
+                   
+                   <li>
+                       <a href="customer_register.php">Registrarme</a>
+                   </li>
+                   <li>
+                       <a href="customer/my_account.php">Mi cuenta</a>
+                   </li>
+                   <li>
+                       <a href="cart.php">Ir al Carrito</a>
+                   </li>
+                   <li>
+                       <a href="cerrar_sesion.php">Login</a>
+                   </li>
+                   
+               </ul><!-- menu termina -->
+               
+           </div><!-- col-md-6 termina -->
+           
+       </div><!-- container termina -->
+       
+   </div><!-- Top termina -->
+   
+   <div id="navbar" class="navbar navbar-default"><!-- navbar navbar-default empieza -->
+       
+       <div class="container"><!-- container empieza -->
+           
+           <div class="navbar-header"><!-- navbar-header empieza -->
+               
+               <a href="index.php" class="navbar-brand home"><!-- navbar-brand home empieza -->
+                   
+               <img src="images\VicentaLogoAjustado.png" alt="VicentaLogo" class="hidden-xs" width=150px height=50px>
+               <img src="images\VicentaLogoAjustado.png" alt="VicentaLogo" class="visible-xs" width=150px height=50px>
+                   
+               </a><!-- navbar-brand home termina -->
+               
+               <button class="navbar-toggle" data-toggle="collapse" data-target="#navigation">
+                   
+                   <span class="sr-only">Buscar</span>
+                   
+                   <i class="fa fa-align-justify"></i>
+                   
+               </button>
+               
+               <button class="navbar-toggle" data-toggle="collapse" data-target="#search">
+                   
+                   <span class="sr-only">Buscar</span>
+                   
+                   <i class="fa fa-search"></i>
+                   
+               </button>
+               
+           </div><!-- navbar-header termina -->
+           
+           <div class="navbar-collapse collapse" id="navigation"><!-- navbar-collapse collapse empieza -->
+               
+               <div class="padding-nav"><!-- padding-nav empieza -->
+                   
+                   <ul class="nav navbar-nav left"><!-- nav navbar-nav left empieza -->
+                       
+                       <li class="<?= $active == 'Inicio' ? 'active' : '' ?>" >
+                           <a href="index.php">Home</a>
+                       </li>
+                       <li class= "<?= $active == 'Comprar' ? 'active' : '' ?>" >
+                           <a href="tienda.php">Comprar</a>
+                       </li>
+
+                       <li class="<?= $active == 'Mi cuenta' ? 'active' : '' ?>">
+
+                            <?php 
+                           
+                             if(!isset($_SESSION['customer_email'])){
+                               
+                               echo"<a href='cerrar_sesion.php'>Mi cuenta</a>";
+                               
+                            }else{
+                               
+                              echo"<a href='my_account.php?my_orders'>Mi cuenta</a>"; 
+                               
+                             }
+                           
+                           ?>
+                           
+                       </li>
+                       <li class="<?= $active == 'Carrito' ? 'active' : '' ?>">
+                           <a href="cart.php">Carrito</a>
+                       </li>
+                       <li class="<?= $active == 'Contactanos' ? 'active' : '' ?>">
+                           <a href="contact.php">Contactanos</a>
+                       </li>
+                       
+                   </ul><!-- nav navbar-nav left termina -->
+                   
+               </div><!-- padding-nav termina -->
+               
+               <a href="cart.php" class="btn navbar-btn btn-primary right"><!-- btn navbar-btn btn-primary empieza -->
+                   
+                   <i class="fa fa-shopping-cart"></i>
+                   
+                   <span><?php items(); ?>  Productos en tu carrito</span>
+                   
+               </a><!-- btn navbar-btn btn-primary termina -->
+               
+               <div class="navbar-collapse collapse right"><!-- navbar-collapse collapse right empieza -->
+                   
+                   <button class="btn btn-primary navbar-btn" type="button" data-toggle="collapse" data-target="#search"><!-- btn btn-primary navbar-btn empieza -->
+                       
+                       <span class="sr-only">Buscar</span>
+                       
+                       <i class="fa fa-search"></i>
+                       
+                   </button><!-- btn btn-primary navbar-btn termina -->
+                   
+               </div><!-- navbar-collapse collapse right termina -->
+               
+               <div class="collapse clearfix" id="search"><!-- collapse clearfix empieza -->
+                   
+                   <form method="get" action="results.php" class="navbar-form"><!-- navbar-form empieza -->
+                       
+                       <div class="input-group"><!-- input-group empieza -->
+                           
+                           <input type="text" class="form-control" placeholder="Search" name="user_query" required>
+                           
+                           <span class="input-group-btn"><!-- input-group-btn empieza -->
+                           
+                           <button type="submit" name="search" value="Search" class="btn btn-primary"><!-- btn btn-primary empieza -->
+                               
+                               <i class="fa fa-search"></i>
+                               
+                           </button><!-- btn btn-primary termina -->
+                           
+                           </span><!-- input-group-btn termina -->
+                           
+                       </div><!-- input-group termina -->
+                       
+                   </form><!-- navbar-form termina -->
+                   
+               </div><!-- collapse clearfix termina -->
+               
+           </div><!-- navbar-collapse collapse termina -->
+           
+       </div><!-- container termina -->
+       
+   </div><!-- navbar navbar-default termina -->
---
+++ admin_area/includes/sidebar.php
@@ -0,0 +1,220 @@
+
+<nav class="navbar navbar-inverse navbar-fixed-top"><!-- navbar navbar-inverse navbar-fixed-top begin -->
+    <div class="navbar-header"><!-- navbar-header begin -->
+        
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse"><!-- navbar-toggle begin -->
+            
+            <span class="sr-only">Alternar navegacion</span>
+            
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            
+        </button><!-- navbar-toggle finish -->
+        
+        <a href="index.php?dashboard" class="navbar-brand">Admin Area</a>
+        
+    </div><!-- navbar-header finish -->
+    
+    <ul class="nav navbar-right top-nav"><!-- nav navbar-right top-nav begin -->
+        
+        <li class="dropdown"><!-- dropdown begin -->
+            
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><!-- dropdown-toggle begin -->
+                
+                <i class="fa fa-user"></i> Vicenta <b class="caret"></b>
+                
+            </a><!-- dropdown-toggle finish -->
+            
+            <ul class="dropdown-menu"><!-- dropdown-menu begin -->
+                <li><!-- li begin -->
+                    <a href="index.php?user_profile"><!-- a href begin -->
+                        
+                        <i class="fa fa-fw fa-user"></i> Perfil
+                        
+                    </a><!-- a href finish -->
+                </li><!-- li finish -->
+                
+                <li><!-- li begin -->
+                    <a href="index.php?view_products"><!-- a href begin -->
+                        
+                        <i class="fa fa-fw fa-envelope"></i> Productos
+                        
+                        <span class="badge">7</span>
+                        
+                    </a><!-- a href finish -->
+                </li><!-- li finish -->
+                
+                <li><!-- li begin -->
+                    <a href="index.php?view_customers"><!-- a href begin -->
+                        
+                        <i class="fa fa-fw fa-users"></i> Clientes
+                        
+                        <span class="badge">11</span>
+                        
+                    </a><!-- a href finish -->
+                </li><!-- li finish -->
+                
+                <li><!-- li begin -->
+                    <a href="index.php?view_cats"><!-- a href begin -->
+                        
+                        <i class="fa fa-fw fa-gear"></i> Producto Categorias 
+                        
+                        <span class="badge">4</span>
+                        
+                    </a><!-- a href finish -->
+                </li><!-- li finish -->
+                
+                <li class="divider"></li>
+                
+                <li><!-- li begin -->
+                    <a href="logout.php"><!-- a href begin -->
+                        
+                        <i class="fa fa-fw fa-power-off"></i> Salir
+                        
+                    </a><!-- a href finish -->
+                </li><!-- li finish -->
+                
+            </ul><!-- dropdown-menu finish -->
+            
+        </li><!-- dropdown finish -->
+        
+    </ul><!-- nav navbar-right top-nav finish -->
+    
+    <div class="collapse navbar-collapse navbar-ex1-collapse"><!-- collapse navbar-collapse navbar-ex1-collapse begin -->
+        <ul class="nav navbar-nav side-nav"><!-- nav navbar-nav side-nav begin -->
+            <li><!-- li begin -->
+                <a href="index.php?dashboard"><!-- a href begin -->
+                        
+                        <i class="fa fa-fw fa-dashboard"></i> Panel
+                        
+                </a><!-- a href finish -->
+                
+            </li><!-- li finish -->
+            
+            <li><!-- li begin -->
+                <a href="#" data-toggle="collapse" data-target="#products"><!-- a href begin -->
+                        
+                        <i class="fa fa-fw fa-tag"></i> Productos
+                        <i class="fa fa-fw fa-caret-down"></i>
+                        
+                </a><!-- a href finish -->
+                
+                <ul id="products" class="collapse"><!-- collapse begin -->
+                    <li><!-- li begin -->
+                        <a href="index.php?insert_product"> Insertar Productos  </a>
+                    </li><!-- li finish -->
+                    <li><!-- li begin -->
+                        <a href="index.php?view_products"> Ver productos  </a>
+                    </li><!-- li finish -->
+                </ul><!-- collapse finish -->
+                
+            </li><!-- li finish -->
+            
+            <li><!-- li begin -->
+                <a href="#" data-toggle="collapse" data-target="#p_cat"><!-- a href begin -->
+                        
+                        <i class="fa fa-fw fa-edit"></i> Productos Categorias
+                        <i class="fa fa-fw fa-caret-down"></i>
+                        
+                </a><!-- a href finish -->
+                
+                <ul id="p_cat" class="collapse"><!-- collapse begin -->
+                    <li><!-- li begin -->
+                        <a href="index.php?insert_p_cat"> Insertar Producto categoria </a>
+                    </li><!-- li finish -->
+                    <li><!-- li begin -->
+                        <a href="index.php?view_p_cats"> ver  Producto categoria </a>
+                    </li><!-- li finish -->
+                </ul><!-- collapse finish -->
+                
+            </li><!-- li finish -->
+            
+            <li><!-- li begin -->
+                <a href="#" data-toggle="collapse" data-target="#cat"><!-- a href begin -->
+                        
+                        <i class="fa fa-fw fa-book"></i> Categorias
+                        <i class="fa fa-fw fa-caret-down"></i>
+                        
+                </a><!-- a href finish -->
+                
+                <ul id="cat" class="collapse"><!-- collapse begin -->
+                    <li><!-- li begin -->
+                        <a href="index.php?insert_cat"> Insertar Categoria </a>
+                    </li><!-- li finish -->
+                    <li><!-- li begin -->
+                        <a href="index.php?view_cats"> ver Categoria </a>
+                    </li><!-- li finish -->
+                </ul><!-- collapse finish -->
+                
+            </li><!-- li finish -->
+            
+            <li><!-- li begin -->
+                <a href="#" data-toggle="collapse" data-target="#slides"><!-- a href begin -->
+                        
+                        <i class="fa fa-fw fa-gear"></i> Carrucel
+                        <i class="fa fa-fw fa-caret-down"></i>
+                        
+                </a><!-- a href finish -->
+                
+                <ul id="slides" class="collapse"><!-- collapse begin -->
+                    <li><!-- li begin -->
+                        <a href="index.php?insert_slide"> Insertar imagen al Carrucel </a>
+                    </li><!-- li finish -->
+                    <li><!-- li begin -->
+                        <a href="index.php?view_slides"> Ver amgenes del carrucel </a>
+                    </li><!-- li finish -->
+                </ul><!-- collapse finish -->
+                
+            </li><!-- li finish -->
+            
+            <li><!-- li begin -->
+                <a href="index.php?view_customers"><!-- a href begin -->
+                    <i class="fa fa-fw fa-users"></i> Ver Clentes
+                </a><!-- a href finish -->
+            </li><!-- li finish -->
+            
+            <li><!-- li begin -->
+                <a href="index.php?view_orders"><!-- a href begin -->
+                    <i class="fa fa-fw fa-book"></i> Ver Ordenes 
+                </a><!-- a href finish -->
+            </li><!-- li finish -->
+            
+            <li><!-- li begin -->
+                <a href="index.php?view_payments"><!-- a href begin -->
+                    <i class="fa fa-fw fa-money"></i> Ver Pagos 
+                </a><!-- a href finish -->
+            </li><!-- li finish -->
+            
+            <li><!-- li begin -->
+                <a href="#" data-toggle="collapse" data-target="#users"><!-- a href begin -->
+                        
+                        <i class="fa fa-fw fa-users"></i> Usuarios
+                        <i class="fa fa-fw fa-caret-down"></i>
+                        
+                </a><!-- a href finish -->
+                
+                <ul id="users" class="collapse"><!-- collapse begin -->
+                    <li><!-- li begin -->
+                        <a href="index.php?insert_user"> Insertar usuario </a>
+                    </li><!-- li finish -->
+                    <li><!-- li begin -->
+                        <a href="index.php?view_users"> Ver Usuario </a>
+                    </li><!-- li finish -->
+                    <li><!-- li begin -->
+                        <a href="index.php?user_profile"> Editar Perfil del usuario  </a>
+                    </li><!-- li finish -->
+                </ul><!-- collapse finish -->
+                
+            </li><!-- li finish -->
+            
+            <li><!-- li begin -->
+                <a href="logout.php"><!-- a href begin -->
+                    <i class="fa fa-fw fa-power-off"></i> Salir
+                </a><!-- a href finish -->
+            </li><!-- li finish -->
+            
+        </ul><!-- nav navbar-nav side-nav finish -->
+    </div><!-- collapse navbar-collapse navbar-ex1-collapse finish -->
+    
+</nav><!-- navbar navbar-inverse navbar-fixed-top finish -->
