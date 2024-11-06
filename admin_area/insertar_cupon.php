9cde53e98c0e99c6732f03e1f9889f370ca3a384
Kendrik Lamar <164172879+NicoSiffredo@users.noreply.github.com>
2024-11-06 20:18:50 -0300

    cupones Parte administrativa

---
+++ admin_area/insertar_cupon.php
@@ -0,0 +1,198 @@
+<?php 
+
+    if(!isset($_SESSION['admin_email'])){
+        
+        echo "<script>window.open('login.php','_self')</script>";
+        
+    }else{
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> insertar cupones </title>
+</head>
+<body>
+    
+<div class="row"><!-- row Begin -->
+    
+    <div class="col-lg-12"><!-- col-lg-12 Begin -->
+        
+        <ol class="breadcrumb"><!-- breadcrumb Begin -->
+            
+            <li class="active"><!-- active Begin -->
+                
+                <i class="fa fa-dashboard"></i> Panel / insertar cupones
+                
+            </li><!-- active Finish -->
+            
+        </ol><!-- breadcrumb Finish -->
+        
+    </div><!-- col-lg-12 Finish -->
+    
+</div><!-- row Finish -->
+       
+<div class="row"><!-- row Begin -->
+    
+    <div class="col-lg-12"><!-- col-lg-12 Begin -->
+        
+        <div class="panel panel-default"><!-- panel panel-default Begin -->
+            
+           <div class="panel-heading"><!-- panel-heading Begin -->
+               
+               <h3 class="panel-title"><!-- panel-title Begin -->
+                   
+                   <i class="fa fa-money fa-fw"></i> insertar cupones
+                   
+               </h3><!-- panel-title Finish -->
+               
+           </div> <!-- panel-heading Finish -->
+           
+           <div class="panel-body"><!-- panel-body Begin -->
+               
+               <form method="post" class="form-horizontal"><!-- form-horizontal Begin -->
+                   
+                   <div class="form-group"><!-- form-group Begin -->
+                       
+                      <label class="col-md-3 control-label"> Cupon Nombre </label> 
+                      
+                      <div class="col-md-6"><!-- col-md-6 Begin -->
+                          
+                          <input name="cupón_titulo" type="text" class="form-control" required>
+                          
+                      </div><!-- col-md-6 Finish -->
+                       
+                   </div><!-- form-group Finish -->
+                   
+                   <div class="form-group"><!-- form-group Begin -->
+                       
+                      <label class="col-md-3 control-label"> Cupon Precio </label> 
+                      
+                      <div class="col-md-6"><!-- col-md-6 Begin -->
+                          
+                          <input name="cupon_precio" type="text" class="form-control" required>
+                          
+                      </div><!-- col-md-6 Finish -->
+                       
+                   </div><!-- form-group Finish -->
+                   
+                   <div class="form-group"><!-- form-group Begin -->
+                       
+                      <label class="col-md-3 control-label"> Copon Limite </label> 
+                      
+                      <div class="col-md-6"><!-- col-md-6 Begin -->
+                          
+                          <input name="cupon_limite" type="number" class="form-control" value="1">
+                          
+                      </div><!-- col-md-6 Finish -->
+                       
+                   </div><!-- form-group Finish -->
+                   
+                   <div class="form-group"><!-- form-group Begin -->
+                       
+                      <label class="col-md-3 control-label"> Seleccionar Producto </label> 
+                      
+                      <div class="col-md-6"><!-- col-md-6 Begin -->
+                          
+                          <select name="producto_id" class="form-control" required>
+                          
+                            <option selected disabled>Selecciona un producto para el cupon</option>
+
+                            <?php 
+                            
+                                $get_p = "select * from productos";
+                                $run_p = mysqli_query($con,$get_p);
+
+                                while($row_p = mysqli_fetch_array($run_p)){
+
+                                    $p_id = $row_p['producto_id'];
+                                    $p_title = $row_p['producto_titulo'];
+
+                                    echo "<option value='$p_id'>$p_title</option>";
+
+                                }
+                            
+                            ?>
+                          
+                          </select>
+                          
+                      </div><!-- col-md-6 Finish -->
+                       
+                   </div><!-- form-group Finish -->
+                   
+                   <div class="form-group"><!-- form-group Begin -->
+                       
+                      <label class="col-md-3 control-label"> Copon Codigo </label> 
+                      
+                      <div class="col-md-6"><!-- col-md-6 Begin -->
+                          
+                          <input name="cupon_codigo" type="text" class="form-control" required>
+                          
+                      </div><!-- col-md-6 Finish -->
+                       
+                   </div><!-- form-group Finish -->
+                   
+                   <div class="form-group"><!-- form-group Begin -->
+                       
+                      <label class="col-md-3 control-label"></label> 
+                      
+                      <div class="col-md-6"><!-- col-md-6 Begin -->
+                          
+                          <input name="submit" value="Create Coupon" type="submit" class="btn btn-primary form-control">
+                          
+                      </div><!-- col-md-6 Finish -->
+                       
+                   </div><!-- form-group Finish -->
+                   
+               </form><!-- form-horizontal Finish -->
+               
+           </div><!-- panel-body Finish -->
+            
+        </div><!-- canel panel-default Finish -->
+        
+    </div><!-- col-lg-12 Finish -->
+    
+</div><!-- row Finish -->
+
+<?php 
+
+if(isset($_POST['submit'])){
+
+    $cupón_titulo = $_POST['cupón_titulo'];
+    $cupon_precio = $_POST['cupon_precio'];
+    $cupon_codigo = $_POST['cupon_codigo'];
+    $cupon_limite = $_POST['cupon_limite'];
+    $coupon_pro_id = $_POST['producto_id'];
+
+    $cupon_usado=0;
+
+    $get_coupons = "select * from coupons where producto_id='$coupon_pro_id' or cupon_codigo='$cupon_codigo'";
+    $run_coupons = mysqli_query($con,$get_coupons);
+    $check_coupons = mysqli_num_rows($run_coupons);
+
+    if($check_coupons==1){
+
+        echo "<script>alert('Cupon codigo / Producto ya añadido. elije otro codigo de cupon / Producto')</script>";
+
+    }else{
+
+        $insert_coupon = "insert into cupon (producto_id,cupón_titulo,cupon_precio,cupon_codigo,cupon_limite,cupon_usado)values('$coupon_pro_id','$cupón_titulo','$cupon_precio','$cupon_codigo','$cupon_limite','$cupon_usado')";
+        $run_coupon = mysqli_query($con,$insert_coupon);
+
+        if($run_coupon){
+
+            echo "<script>alert('El cupon se ha creado')</script>";
+            echo "<script>window.open('index.php?ver_cupones','_self')</script>";
+
+        }
+
+    }
+
+}
+
+?>
+
+<?php } ?>
